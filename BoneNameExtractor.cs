c1444f1a2e29565d12879b13554194dffe00841b
Azukimochi <Azukimochi25565@gmail.com>
2023-03-28 15:40:07 +0900

    init

---
+++ BoneNameExtractor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BoneNameExtractor
+{
+
+    public class BoneNameExtractor : MonoBehaviour { }
+
+
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(BoneNameExtractor))]
+    public class BoneNameExtractorUI : Editor
+    {
+        public Animator HumanoidAnimator;
+        string logs;
+
+        public override void OnInspectorGUI()
+        {
+            //元のInspector部分を表示
+            base.OnInspectorGUI();
+
+            //ボタンを表示
+            if (GUILayout.Button("実行"))
+            {
+                BoneNameExtractor bnExtracotr = target as BoneNameExtractor;
+                HumanoidAnimator = bnExtracotr.GetComponent<Animator>();
+
+                if (HumanoidAnimator == null)
+                {
+                    Debug.Log("AnimatorIsNull");
+                    return;
+                }
+                if (HumanoidAnimator.isHuman == false)
+                {
+                    Debug.Log("AvatarIsNotHuman");
+                    return;
+                }
+                BoneNameExtract();
+            }
+        }
+
+        private void BoneNameExtract()
+        {
+
+            logs = $"<NAME>{HumanoidAnimator.name}</NAME>";
+
+            foreach (HumanBodyBones bone in Enum.GetValues(typeof(HumanBodyBones)))
+            {
+                Transform boneTf;
+                try
+                {
+                    boneTf = HumanoidAnimator.GetBoneTransform(bone);
+                }catch(IndexOutOfRangeException e)
+                {
+                    boneTf = null;
+                }
+                String boneID = bone.ToString();
+                String boneName = boneTf != null ? boneTf.name : "NONE";
+
+                logs = logs + $"<{boneID}>{boneName}</{boneID}>\n";
+            }
+
+            Debug.Log(logs);
+        }
+    }
+    #endif
+}
